8d5e222c09c3b2199186601d30bbfa67f1799165
Clemence Fernet <clemence.fernet@hotmail.fr>
2017-11-12 22:35:04 +0100

    updated zoom & size

--- source/javascripts/maps.js
+++ source/javascripts/maps.js
@@ -2,7 +2,7 @@
 function initMap() {
         var uluru = {lat: 20.869658, lng: -105.440604};
         var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 10,
+          zoom: 9,
           center: uluru
         });
         var marker = new google.maps.Marker({
